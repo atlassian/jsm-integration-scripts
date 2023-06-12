c4d8e2180e0264cb2dd2ed3e64dad1c0735d1357
Nurettin Akyüz <akyuznurettin@users.noreply.github.com>
2023-06-12 15:49:36 +0300

    Update JEC integration script (#3)

--- jec/scripts/jec_action_executor.py
+++ jec/scripts/jec_action_executor.py
@@ -1,1 +1,13 @@
-print("This is an example script. You can write your custom scripts!")
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-payload', '--payload', help='Payload from queue', required=True)
+
+args, unknown = parser.parse_known_args()
+args = vars(args)
+
+queue_message_string = args['payload']
+queue_message = json.loads(queue_message_string)
+
+print(queue_message)
