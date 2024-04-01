a6d13cde1f0f06f7d86a560bb9e633d1eb118a10
Özlem Çökük <ocokuk@atlassian.com>
2024-04-01 23:24:49 +0200

    set nagios server if it is configured in config file

--- nagios/scripts/jec_action_executor.py
+++ nagios/scripts/jec_action_executor.py
@@ -64,7 +64,14 @@ def parse_from_details(key):
 
 def get_url(conf_property, backward_compatibility_url):
     url = parse_field(conf_property, True)
-    if url:
+    if nagios_server and "default" != nagios_server:
+            scheme = parse_field("scheme", False)
+            if scheme is None and "http" not in nagios_server:
+                scheme = "http"
+            url = urllib.parse.urlunparse(
+                (scheme, nagios_server, urllib.parse.quote(backward_compatibility_url), None, None, None))
+            return url
+    elif url:
         return url
     else:
         # backward compatibility
